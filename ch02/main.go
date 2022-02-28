56c7a8c0d20c3f8127e1becdc7aa7f8f326baf7c
lsytj0413 <511121939@qq.com>
2022-02-28 21:31:40 +0800

    feat(*): add matrix example

--- ch02/main.go
+++ ch02/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
 
 	"gonum.org/v1/gonum/mat"
 )
@@ -10,8 +12,49 @@ func createDataSet() (mat.Matrix, []string) {
 	return mat.NewDense(4, 2, []float64{1.0, 1.1, 1.0, 1.0, 0, 0, 0, 0.1}), []string{"A", "A", "B", "B"}
 }
 
+func classify0(in []float64, dataSet mat.Matrix, labels []string, k int) string {
+	dataSetSize, _ := dataSet.Dims()
+	tileMat := mat.NewDense(dataSetSize, len(in), nil)
+	for r := 0; r < tileMat.RawMatrix().Rows; r++ {
+		tileMat.SetRow(r, in)
+	}
+	fmt.Printf("tileMat: %v\n", mat.Formatted(tileMat))
+	tileMat.Sub(tileMat, dataSet)
+	fmt.Printf("diffMat: %v\n", mat.Formatted(tileMat))
+
+	tileMat.Apply(func(i, j int, v float64) float64 {
+		return v * v
+	}, tileMat)
+	fmt.Printf("diffMat: %v\n", mat.Formatted(tileMat))
+	sqDistance := mat.NewDense(dataSetSize, 1, nil)
+	for r := 0; r < tileMat.RawMatrix().Rows; r++ {
+		d := float64(0)
+		row := tileMat.RowView(r)
+		for c := 0; c < row.Len(); c++ {
+			d += row.At(c, 0)
+		}
+
+		sqDistance.SetRow(r, []float64{d})
+	}
+	fmt.Printf("sqDistance: \n%v\n", mat.Formatted(sqDistance))
+
+	sqDistance.Apply(func(i, j int, v float64) float64 {
+		return math.Sqrt(v)
+	}, sqDistance)
+	fmt.Printf("sqDistance: \n%v\n", mat.Formatted(sqDistance))
+
+	distance := make([]float64, sqDistance.RawMatrix().Rows)
+	copy(distance, sqDistance.RawMatrix().Data)
+
+	sort.Sort(sort.Float64Slice(distance))
+
+	return ""
+}
+
 func main() {
 	group, labels := createDataSet()
-	fmt.Printf("%+v\n", group)
+	fmt.Printf("%v\n", mat.Formatted(group))
 	fmt.Println(labels)
+
+	fmt.Printf("classify: %v\n", classify0([]float64{0.0, 0.0}, group, labels, 1))
 }
